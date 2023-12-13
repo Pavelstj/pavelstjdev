1b624702a67745daf9029e91ffd8a2f458377a2b
pskur <pavel.skuratovets@solbeg.com>
2023-12-13 20:52:43 +0300

    add hashrouter

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -5,7 +5,7 @@ import ExperiencePage from "./experience/ExperiencePage";
 // import PortfolioPage from "./portfolio/PortfolioPage";
 import ContactsPage from "./contacts/ContactsPage";
 import NotFoundPage from "./notfound/NotfoundPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 
 const Content = () => {
@@ -13,16 +13,18 @@ const Content = () => {
   
     return (
         <div className='app-content'>
-          <Routes>
-            <Route path='/'>
-              <Route index element={<HomePage/>}/>
-              <Route path='/about' element={<AboutPage/>}/>
-              <Route path='/experience' element={<ExperiencePage/>}/>
-              {/* <Route path='/portfolio' element={<PortfolioPage/>}/> */}
-              <Route path='/contacts' element={<ContactsPage/>}/>
-              <Route path='*' element={<NotFoundPage/>}/>
-            </Route>
-          </Routes>
+          <HashRouter basename='/pavelstjdev'>
+            <Routes>
+              <Route path='/'>
+                <Route index element={<HomePage/>}/>
+                <Route path='/about' element={<AboutPage/>}/>
+                <Route path='/experience' element={<ExperiencePage/>}/>
+                {/* <Route path='/portfolio' element={<PortfolioPage/>}/> */}
+                <Route path='/contacts' element={<ContactsPage/>}/>
+                <Route path='*' element={<NotFoundPage/>}/>
+              </Route>
+            </Routes>
+          </HashRouter>
         </div>
     );
 }
