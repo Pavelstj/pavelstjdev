4be5a46cbc976248323d41c51f04bd7f1b78bf8e
pskur <pavel.skuratovets@solbeg.com>
2023-12-18 08:59:57 +0300

    fix gh routes

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -5,7 +5,7 @@ import ExperiencePage from './experience/ExperiencePage';
 // import PortfolioPage from './portfolio/PortfolioPage';
 import ContactsPage from './contacts/ContactsPage';
 import NotFoundPage from './notfound/NotfoundPage';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 
 const Content = () => {
@@ -13,18 +13,17 @@ const Content = () => {
   
     return (
         <div className='app-content'>
-          <HashRouter basename='/pavelstjdev'>
-            <Routes>
-              <Route path='/'>
-                <Route index element={<HomePage/>}/>
-                <Route path='/about' element={<AboutPage/>}/>
-                <Route path='/experience' element={<ExperiencePage/>}/>
-                {/* <Route path='/portfolio' element={<PortfolioPage/>}/> */}
-                <Route path='/contacts' element={<ContactsPage/>}/>
-                <Route path='*' element={<NotFoundPage/>}/>
-              </Route>
-            </Routes>
-          </HashRouter>
+          <Routes>
+            <Route path='/'>
+              <Route index element={<HomePage/>}/>
+              <Route path='/pavelstjdev' element={<HomePage/>}/>
+              <Route path='/about' element={<AboutPage/>}/>
+              <Route path='/experience' element={<ExperiencePage/>}/>
+              {/* <Route path='/portfolio' element={<PortfolioPage/>}/> */}
+              <Route path='/contacts' element={<ContactsPage/>}/>
+              <Route path='*' element={<NotFoundPage/>}/>
+            </Route>
+          </Routes>
         </div>
     );
 }
